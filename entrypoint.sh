d0c26f1f22fcc4e831ac1ee859ede3ff898b1ad4
Jeremy Katz <jeremy@tidelift.com>
2022-07-07 18:07:21 -0400

    Add support for running in a MacOS runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,17 @@ set -o pipefail
 set -x
 
 echo "Downloading latest CLI"
-curl -s -o ./tidelift https://download.tidelift.com/cli/tidelift
+case "$(uname -s)" in
+Linux)
+    curl -s -o ./tidelift https://download.tidelift.com/cli/tidelift
+    ;;
+Darwin)
+    curl -s -o ./tidelift https://download.tidelift.com/cli/tidelift_darwin
+    ;;
+*)
+    echo "Unable to detect OS. Please contact Tidelift support at support@tidelift.com"
+    ;;
+esac
 chmod +x ./tidelift
 
 echo "Current Tidelift CLI version"
